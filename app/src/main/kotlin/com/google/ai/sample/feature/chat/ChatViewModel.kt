3d2de121ded28cceaa5984f296f0f5394440e36a
Nishan <157925518+nishan2293@users.noreply.github.com>
2024-06-02 22:31:55 -0500

    Add files via upload

---
+++ app/src/main/kotlin/com/google/ai/sample/feature/chat/ChatViewModel.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.ai.sample.feature.chat
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.asTextOrNull
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class ChatViewModel(
+    generativeModel: GenerativeModel
+) : ViewModel() {
+    private val chat = generativeModel.startChat(
+    )
+
+    private val _uiState: MutableStateFlow<ChatUiState> =
+        MutableStateFlow(ChatUiState(chat.history.map { content ->
+            // Map the initial messages
+            ChatMessage(
+                text = content.parts.first().asTextOrNull() ?: "",
+                participant = if (content.role == "user") Participant.USER else Participant.TUTOR,
+                isPending = false
+            )
+        }))
+    val uiState: StateFlow<ChatUiState> =
+        _uiState.asStateFlow()
+
+
+    fun sendMessage(userMessage: String) {
+        // Add a pending message
+        _uiState.value.addMessage(
+            ChatMessage(
+                text = userMessage,
+                participant = Participant.USER,
+                isPending = true
+            )
+        )
+
+        viewModelScope.launch {
+            try {
+                chat.sendMessage("You're a kids tutor for kids aged 2-7 years. Chat with the kid (no explicit content, use a lot of emojis in your response, response should be max 50 words)")
+                val response = chat.sendMessage(userMessage)
+
+                _uiState.value.replaceLastPendingMessage()
+
+                response.text?.let { modelResponse ->
+                    _uiState.value.addMessage(
+                        ChatMessage(
+                            text = modelResponse.replace("*", ""),
+                            participant = Participant.TUTOR,
+                            isPending = false
+                        )
+                    )
+                }
+            } catch (e: Exception) {
+                _uiState.value.replaceLastPendingMessage()
+                _uiState.value.addMessage(
+                    ChatMessage(
+                        text = e.localizedMessage,
+                        participant = Participant.ERROR
+                    )
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/kotlin/com/google/ai/sample/feature/practice/PracticeViewModel.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.ai.sample.feature.practice
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.asTextOrNull
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class PracticeViewModel(
+    generativeModel: GenerativeModel
+) : ViewModel() {
+    private val chat = generativeModel.startChat(
+        history = listOf(
+            content(role = "model") { text("Give me a topic, and I'll ask you questions on that topic.") },
+            )
+    )
+
+    private val _uiState: MutableStateFlow<ChatUiState> =
+        MutableStateFlow(ChatUiState(chat.history.map { content ->
+            // Map the initial messages
+            ChatMessage(
+                text = content.parts.first().asTextOrNull() ?: "",
+                participant = if (content.role == "user") Participant.USER else Participant.TUTOR,
+                isPending = false
+            )
+        }))
+    val uiState: StateFlow<ChatUiState> =
+        _uiState.asStateFlow()
+
+
+    fun sendMessage(userMessage: String) {
+        // Add a pending message
+        _uiState.value.addMessage(
+            ChatMessage(
+                text = userMessage,
+                participant = Participant.USER,
+                isPending = true
+            )
+        )
+
+        viewModelScope.launch {
+            try {
+                chat.sendMessage("Check my answers very strictly and assume I'm a 2-7 year kid. Use max emojis to explain response")
+                val response = chat.sendMessage(userMessage)
+
+                _uiState.value.replaceLastPendingMessage()
+
+                response.text?.let { modelResponse ->
+                    _uiState.value.addMessage(
+                        ChatMessage(
+                            text = modelResponse.replace("*", ""),
+                            participant = Participant.TUTOR,
+                            isPending = false
+                        )
+                    )
+                }
+            } catch (e: Exception) {
+                _uiState.value.replaceLastPendingMessage()
+                _uiState.value.addMessage(
+                    ChatMessage(
+                        text = e.localizedMessage,
+                        participant = Participant.ERROR
+                    )
+                )
+            }
+        }
+    }
+}
